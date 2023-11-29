4fa866b8fdc1ef80446d24a3f760caefcab10b4c
Olga <o.pogibelnaya@g.nsu.ru>
2023-11-30 02:35:14 +0700

    Сделала вход в гугл-аккаунт

--- enrollease-frontend/src/App.vue
+++ enrollease-frontend/src/App.vue
@@ -1,10 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld/>
+  <div v-if="loggedIn">
+    <button @click="logout">logout</button>
+    <h1>Hello, {{ user.name }}</h1>
+    <img :src="user.picture">
+    <h2>The name is: {{ user.name }}</h2>
+    <h2>The email is: {{ user.email }}</h2>
+  </div>
+  <div v-else>
+    <GoogleLogin :callback="callback" prompt auto-login />
+  </div>
 </template>
 
-<style scoped>
-</style>
+
+<script setup>
+import { ref } from 'vue'
+import { GoogleLogin, decodeCredential, googleLogout } from 'vue3-google-login';
+//import HelloWorld from './components/HelloWorld.vue'
+
+const loggedIn = ref(false)
+const user = ref(null)
+
+function callback(response) {
+  loggedIn.value = true
+  user.value = decodeCredential(response.credential)
+}
+
+function logout() {
+  googleLogout()
+  loggedIn.value = false
+}
+</script>
+
+
+<style scoped></style>
--- enrollease-frontend/src/main.js
+++ enrollease-frontend/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import vue3GoogleLogin from 'vue3-google-login'
 
-createApp(App).mount('#app')
+const CLIENT_ID = ""
+
+createApp(App)
+.use(vue3GoogleLogin, {
+    clientId: CLIENT_ID,
+}).mount('#app')
